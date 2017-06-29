ec69c00182b62ffdecb067d311b2c65b69b49de4
Alexander Ayala <nikk35p@gmail.com>
2017-06-29 18:46:34 -0500

    Iniciamos

---
+++ resources/views/store/partials/footer.blade.php
@@ -0,0 +1,38 @@
+<footer class="container-fluid">
+	<div class="row">
+		<div class="col-md-4">
+			<h3>Mi Almacen Laravel</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet quia officia soluta dolorem dolorum sapiente ex eligendi accusantium perspiciatis illo! Aperiam aliquid impedit enim assumenda repudiandae temporibus, nemo eum aspernatur ipsam modi dolorum ut, quae veritatis nobis, unde numquam quod incidunt debitis rem. Odit numquam aliquid incidunt, eveniet maiores culpa.</p>
+		</div>
+		<div class="col-md-4">
+			<h3>Desarrollado por:</h3>
+			<div class="author-info">
+				<img src="https://pbs.twimg.com/profile_images/1997679108/click_bigger.jpg" alt="" class="avatar">
+				<p><a href="@alejoayala"></a>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis quos culpa qui voluptate aliquam alias, adipisci ex distinctio libero et ab saepe tempora recusandae molestiae accusamus laborum fuga autem iure.</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<h3>Siguenos</h3>
+			<ul class="redes">
+				<li>
+					<a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-youtube-square fa-2x"></i></a>
+				</li>
+				
+			</ul>
+			<h3>Escribenos</h3>
+			<i class="fa fa-at"></i><a href="#">nikk35p@gmail.com</a>
+		</div>
+	</div>
+</footer>
