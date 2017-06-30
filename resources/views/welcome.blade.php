ad53e07bfbe4fc122be8769d2e9f15723491fb04
Alexander Ayala <nikk35p@gmail.com>
2017-06-29 21:18:32 -0500

    cambio desde Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Empezamos con Laravel 5 desde Test</div>
+                <div class="title">Empezamos con Laravel 5 desde Test y un cambio mas</div>
             </div>
         </div>
     </body>
